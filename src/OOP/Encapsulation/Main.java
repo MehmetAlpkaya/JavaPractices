ce2bbe42a8726bf44ed42a313b64e1d7384fd143
Mehmet Alpkaya <alpkaya71@gmail.com>
2023-04-09 00:56:54 +0300

    Encapsulation

---
+++ src/OOP/Encapsulation/Main.java
@@ -0,0 +1,10 @@
+package OOP.Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        Book herryPotter=new Book("Herry Potter", 100);
+        System.out.println(herryPotter.getPageNumber());
+        
+    }
+
+}
